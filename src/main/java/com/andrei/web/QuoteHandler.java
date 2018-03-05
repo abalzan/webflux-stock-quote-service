3f24c54b59f21f5bb8b809d8ffb8bffd73039342
Andrei <andreibalzan@gmail.com>
2018-03-05 14:07:48 +0100

    creating Handler and router reactive function

---
+++ src/main/java/com/andrei/web/QuoteHandler.java
@@ -0,0 +1,32 @@
+package com.andrei.web;
+
+import java.time.Duration;
+
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import com.andrei.model.Quote;
+import com.andrei.service.QuoteGeneratorService;
+
+import reactor.core.publisher.Mono;
+
+@Component
+public class QuoteHandler {
+
+	private final QuoteGeneratorService quoteGeneratorService;
+
+	public QuoteHandler(QuoteGeneratorService quoteGeneratorService) {
+		this.quoteGeneratorService = quoteGeneratorService;
+	}
+
+	public Mono<ServerResponse> fetchQuote(ServerRequest request) {
+		int size = Integer.parseInt(request.queryParam("size").orElse("10"));
+
+		return ServerResponse.ok()
+							 .contentType(MediaType.APPLICATION_JSON)
+							 .body(this.quoteGeneratorService.fetchQuoteStream(Duration.ofMillis(100)).take(size), Quote.class);
+	}
+
+}
